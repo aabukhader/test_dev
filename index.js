cdb91d1acdd566f232d41c9ceeb2aea56559b227
aabukhader <abdallhabukhader@gmail.com>
2026-02-05 21:49:29 +0300

    Initial commit

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Main entry point for shared ESLint configuration
- * This is the base configuration that can be extended by projects
- */
-module.exports = {
-  extends: [
-    './eslint-base.js'
-  ]
-};
